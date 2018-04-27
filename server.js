90f41a73abdba5d0b568574258e0b0f7aacab291
Dominic Ajagba <DominicAjagba@gmail.com>
2018-04-27 17:42:28 -0400

    Login system update and api.js created

--- server.js
+++ server.js
@@ -3,17 +3,26 @@
 const express = require('express');
 const http = require('http')
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//Include api calls 
+const api = require('./routes/api')
 
+//Used for login system 
+app.use(bodyParser.json())
+app.use('/api',api)
+//Paths to static file 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(__dirname + '/assets'));
 
+//Getting the index.html file from the dist folder (Created after running ng serve --aot -prod)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+//Set port to listen on with response 
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
