19b67829e98340c27b323f01ff07c75bb205c299
Dominic Ajagba <DominicAjagba@gmail.com>
2018-04-27 15:29:06 -0400

    header image fix

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use("/assets", express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/assets'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
