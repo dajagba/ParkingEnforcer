a06102f639992d715d1f63c20f5639fd80a997b5
zbaker94 <zbaker94@gmail.com>
2018-04-28 21:23:18 -0400

    add routes for aboutus and profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { LoginComponent} from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import { ProfileComponent } from './profile/profile.component';
 import { AuthGuardService } from './auth-guard.service';
+
 
 const routes: Routes = [
   {
@@ -26,6 +29,16 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+
+  {
+    path: 'aboutus',
+    component: AboutusComponent
+  },
+
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 
 ];
