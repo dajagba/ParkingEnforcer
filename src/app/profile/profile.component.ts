702913a512be1385d6b4d2776c71ad7658e5b898
Dominic Ajagba <DominicAjagba@gmail.com>
2018-04-28 21:59:33 -0400

    Functioning profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticationService, UserDetails } from '../authentication.service'
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  details: UserDetails;
+  constructor(private auth: AuthenticationService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {    
+    this.auth.profile().subscribe(user => {
+      this.details = user;
+    }, (err) => {
+      console.error(err);
+    });
   }
 
 }
