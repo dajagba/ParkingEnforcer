176f1a2ecf5b23e6a8a16c46f417c346ff206d47
Dominic Ajagba <DominicAjagba@gmail.com>
2018-04-30 05:32:35 -0400

    Revised accordion data pull

--- app_api/controllers/dashboard.js
+++ app_api/controllers/dashboard.js
@@ -10,7 +10,7 @@ var registeredVehicles = mongoose.model('registeredVehicles');
  */
 module.exports.getVehiclesInLot = function (req, res) {
     vehiclesInLot.find(function (err, vehiclesinlot) {
-        res.setHeader('content-type', 'text/javascript');
+        res.setHeader('content-type', 'text/plain');
         res.send(vehiclesinlot);
     });
 };
